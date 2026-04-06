ecfbb4614fb051c3553033914ff6d5d8e0d64972
Your Name <you@example.com>
2026-04-06 16:57:30 +0700

    fixed app crashed

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getBooks, createBook, updateBookPrice } = require("../controller/bookController");
+const {
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+  updateBookPrice,
+} = require("../controller/bookController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
 router.get("/", getBooks);
@@ -8,6 +14,5 @@ router.post("/", protect, authorize("admin"), createBook);
 router.put("/:id", protect, authorize("admin"), updateBook);
 router.delete("/:id", protect, authorize("admin"), deleteBook);
 router.patch("/:id/price", protect, authorize("admin"), updateBookPrice);
-router.delete("/:id", protect, authorize("admin"), deleteBook);
 
 module.exports = router;
